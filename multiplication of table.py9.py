098413e57699b32c801fc9d988318c7cc4b7b463
192011241 <112851388+192011241@users.noreply.github.com>
2022-09-07 12:56:23 +0530

    Add files via upload

---
+++ multiplication of table.py9.py
@@ -0,0 +1,7 @@
+# multiplication table (from 1to 10)
+num=25
+#num=int(input("display multiplaction table of num"))
+
+#repeat 10 times from i=1to10
+for i in range(1,11):
+    print(num,'x',i,'=',num*i)
