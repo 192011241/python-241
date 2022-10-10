6f476ae066813a4487bda65f27a9f600c2a729c0
192011241 <112851388+192011241@users.noreply.github.com>
2022-10-10 13:21:48 +0530

    Add files via upload

---
+++ day 5 lengthoflastwords.py
@@ -0,0 +1,17 @@
+=int(input("enter the value of b"))
+if(a<0):
+    print('invalid input')
+elif(b<0):
+    print('invalid input')
+elif(a==b):
+    print("enter differnt numbers")
+elif(a>b):
+    print("enter a is smaller number than b")
+    
+for i in range (a+1,b+1):
+    com=0
+    for j in range(1,b+1):
+        if(i%j==0):
+            com=com+1
+    if(com>2):
+        print(i ,end=" ")
