5b1d465208be5480015b7e4802fffed724a569be
192011241 <112851388+192011241@users.noreply.github.com>
2022-10-10 13:23:22 +0530

    Add files via upload

---
+++ day 4 constants and vowels.py
@@ -0,0 +1,28 @@
+string = input('String: ')
+print('Vowels: ')
+for ch in string:
+   if ch in "AEIOUaeiou":
+      print(ch, end=' ')
+print('\nConsonants: ')
+for ch in string:
+   if ch not in "AEIOUaeiou ":
+      print(ch, end=' ')
+a=int(input("Total users:"))
+if(a<0):
+    print("INVALID INPUT")
+elif(a==0):
+    print("There is no staff and student user")
+else:
+    b=int(input("Staff users:"))
+    if(b<0):
+        print("INVALID INPUT")
+    elif(b==0):
+        print("Student user =",a)
+    elif(a<b):
+        print("INVALID INPUT")
+    elif(a==b):
+        print("Student users =0")
+    else:
+        c=a-b
+        d=c-b/3
+        print("Student users =",int(d))
